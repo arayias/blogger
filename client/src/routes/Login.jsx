13a0a3f20c3087be6ea730b26c398c09ef141c2f
Andreas Rayias <wowcaeu.andreas@gmail.com>
2023-10-12 00:33:07 +0100

    added login page

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,8 +1,34 @@
 export default function Login() {
   return (
-    <div id="login">
-      <h1>Login</h1>
-      <p>TODO: Login form</p>
+    <div id="login" className="flex flex-col items-center content-center">
+      <form
+        action="/login"
+        method="POST"
+        className="flex flex-col items-center p-2 rounded-md shadow-md bg-slate-50"
+      >
+        <label className="flex flex-col items-left min-w[1/2]">
+          <span className="mx-2 mt-1 font-semibold">Username</span>
+          <input
+            className="p-2 m-2 border-2 border-gray-300 rounded-md"
+            type="text"
+            id="username"
+          />
+        </label>
+        <label className="flex flex-col items-left">
+          <span className="mx-2 mt-1 font-semibold">Password</span>
+          <input
+            className="p-2 m-2 border-2 border-gray-300 rounded-md"
+            type="password"
+            id="password"
+          />
+        </label>
+        <button
+          type="submit"
+          className="p-2 m-2 transition duration-300 ease-in-out border-2 border-gray-300 rounded-md hover:bg-gray-300"
+        >
+          Login
+        </button>
+      </form>
     </div>
   );
 }
